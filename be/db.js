3f0bbcbed6b1cea8c0dfd29363ddd1b1b81d7273
PrompipatJa <prompipat18348@gmail.com>
2025-06-19 16:31:04 +0700

    finished docker compose

--- be/db.js
+++ be/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg')
 
 const pool = new Pool ({
   user: process.env.PGUSER,
-  host: 'host.docker.internal',
+  host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT
